e3797a02909c4f8455e4b5f7844c5d6b21776a2a
errorcode-4o4 <avinashk.ug20.ee@nitp.ac.in>
2022-04-11 16:11:30 +0530

    first commit

---
+++ question04.java
@@ -0,0 +1,8 @@
+public class question04 {
+    public static void main(String[] args) {
+        float c = (float) 101.10;
+        Float obj = Float.valueOf(c);
+        System.out.println("Initial value : " + c);
+        System.out.println("After Conversion : " + obj);
+    }
+}
